3ac1efd444e2d202c13823334a722ca1afb585e6
Raúl Avila <raul.avila.9604@gmail.com>
2023-10-11 12:30:54 -0300

    código modificado, se aplico ciclo, math, variables, objetos y array, y entre otras condiciones requeridas.

--- js/script.js
+++ js/script.js
@@ -1,53 +1,94 @@
+// Definir preguntas y respuestas
+const preguntas = [
+    {
+        pregunta: "¿Qué selección nunca ganó una Copa América?",
+        respuestas: [
+            { letra: "a", respuesta: "Colombia" },
+            { letra: "b", respuesta: "Bolivia" },
+            { letra: "c", respuesta: "Jamaica" }
+        ],
+        respuestaCorrecta: "c"
+    },
+    {
+        pregunta: "¿Cual es el equipo con más Copas Libertadores de América?",
+        respuestas: [
+            { letra: "a", respuesta: "Peñarol" },
+            { letra: "b", respuesta: "Fluminense" },
+            { letra: "c", respuesta: "Independiente de Avellaneda" }
+        ],
+        respuestaCorrecta: "c"
+    },
+    {
+        pregunta: "¿Quién es el máximo goleador en Mundiales?",
+        respuestas: [
+            { letra: "a", respuesta: "Klose (Alemania)" },
+            { letra: "b", respuesta: "Pelé (Brasil)" },
+            { letra: "c", respuesta: "Ronaldo (Brasil)" }
+        ],
+        respuestaCorrecta: "a"
+    },
+    {
+        pregunta: "¿Quién es el máximo goleador en la historia de la Copa Libertadores?",
+        respuestas: [
+            { letra: "a", respuesta: "Alberto Spencer (Ecuador)" },
+            { letra: "b", respuesta: "Fernando Morena (Uruguay)" },
+            { letra: "c", respuesta: "Diego Maradona (Argentina)" }
+        ],
+        respuestaCorrecta: "a"
+    },
+    {
+        pregunta: "¿Cuál es el actual Campeón del Mundo?",
+        respuestas: [
+            { letra: "a", respuesta: "Alemania" },
+            { letra: "b", respuesta: "Argentina" },
+            { letra: "c", respuesta: "Chile" }
+        ],
+        respuestaCorrecta: "b"
+    }
+];
+
+// Función para mezclar las preguntas
+function mezclarPreguntas(preguntas) {
+    for (let i = preguntas.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [preguntas[i], preguntas[j]] = [preguntas[j], preguntas[i]];
+    }
+    return preguntas;
+}
+
 // Función para ejecutar el juego de quiz
 function jugarQuiz() {
     let puntaje = 0;
+    const preguntasMezcladas = mezclarPreguntas([...preguntas]);
 
-    // Pregunta 1
-    const respuesta1 = prompt("¿Qué equipo nunca ganó una Copa América?\n (a) Colombia\n (b) Bolivia\n (c) Jamaica");
-    if (respuesta1.toLowerCase() === "c" || respuesta1.toLowerCase() === "jamaica") {
-        puntaje++;
-        alert("¡Respuesta correcta!");
-    } else {
-        alert("Respuesta incorrecta. La respuesta correcta es: (c) Jamaica");
+    for (let i = 0; i < preguntasMezcladas.length; i++) {
+        const preguntaActual = preguntasMezcladas[i];
+        const respuesta = prompt(`${preguntaActual.pregunta}\n${preguntaActual.respuestas.map(r => `(${r.letra}) ${r.respuesta}`).join('\n')}`);
+
+        if (respuesta === null) {
+            // El usuario ha cancelado el juego
+            mostrarMensajeFinal(puntaje, true);
+            return;
+        }
+
+        if (respuesta && respuesta.toLowerCase() === preguntaActual.respuestaCorrecta) {
+            puntaje++;
+            alert("¡Respuesta correcta!");
+        } else {
+            alert(`Respuesta incorrecta. La respuesta correcta es: (${preguntaActual.respuestaCorrecta}) ${preguntaActual.respuestas.find(r => r.letra === preguntaActual.respuestaCorrecta).respuesta}`);
+        }
     }
 
-    // Pregunta 2
-    const respuesta2 = prompt("¿Cual es el equipo con más Copas Libertadores de América?\n(a) Peñarol \n(b) Fluminense \n(c) Independiente de Avellaneda");
-    if (respuesta2.toLowerCase() === "c" || respuesta1.toLowerCase() === "independiente de avellaneda") {
-        puntaje++;
-        alert("¡Respuesta correcta!");
-    } else {
-        alert("Respuesta incorrecta. La respuesta correcta es: (c) Independiente de Avellaneda");
-    }
+    mostrarMensajeFinal(puntaje, false);
+}
 
-    // Pregunta 3
-    const respuesta3 = prompt("¿Quién es el máximo goleador en Mundiales?\n(a) Klose (Alemania) \n(b) Pelé (Brasil) \n(c) Ronaldo (Brasil)");
-    if (respuesta3.toLowerCase() === "a" || respuesta3.toLowerCase() === "klose (alemania)") {
-        puntaje++;
-        alert("¡Respuesta correcta!");
+// Función para mostrar el mensaje final
+function mostrarMensajeFinal(puntaje, cancelado) {
+    if (cancelado) {
+        alert("El juego ha sido cancelado.");
     } else {
-        alert("Respuesta incorrecta. La respuesta correcta es: (a) Klose (Alemania)");
+        alert(`El juego ha finalizado. ¡Gracias por jugar!\nPuntaje total: ${puntaje} de 5`);
     }
-
-    // Pregunta 4
-    const respuesta4 = prompt("¿Quién es el máximo goleador en la historia de la Copa Libertadores?\n(a) Alberto Spencer (Ecuador) \n(b) Fernando Morena (Uruguay) \n(c) Diego Maradona (Argentina)");
-    if (respuesta4.toLowerCase() === "a" || respuesta4.toLowerCase() === "alberto spencer (ecuador)") {
-        puntaje++;
-        alert("¡Respuesta correcta!");
-    } else {
-        alert("Respuesta incorrecta. La respuesta correcta es: (a) Alberto Spencer (Ecuador)");
-    }
-
-    // Pregunta 5
-    const respuesta5 = prompt("¿Cuál es el actual Campeón del Mundo'?\n(a) Alemania \n(b) Argentina \n(c) Chile");
-    if (respuesta5.toLowerCase() === "b" || respuesta5.toLowerCase() === "argentina") {
-        puntaje++;
-        alert("¡Respuesta correcta!");
-    } else {
-        alert("Respuesta incorrecta. La respuesta correcta es: (b) Argentina");
-    }
-
-    console.log("Juego terminado. Tu puntaje es: " + puntaje + " de 5");
 }
 
 // Iniciar el juego al cargar la página
